d96f7550a68a98407585276c3734499f2f5ba608
Sueda Yasar <suedayasar97@gmail.com>
2026-09-02 16:05:30 +0300

    Phase 6: Portfolyo CMS backend completed (Auth, Multer, Pagination, Rate Limit)

--- hafta_4/portfolio_cms/index.js
+++ hafta_4/portfolio_cms/index.js
@@ -3,9 +3,35 @@ const pool = require("./db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    error: "Too many requests from this IP, please try again after 15 minutes",
+  },
+});
+
+app.use(limiter);
+
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const app = express();
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 const PORT = process.env.PORT || 3000;
 
@@ -89,6 +115,57 @@ app.get("/api/admin/dashboard", verifyToken, (req, res) => {
     user: req.user,
   });
 });
+
+app.get("/api/projects", async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const offset = (page - 1) * limit;
+
+    const allProjects = await pool.query(
+      "SELECT * FROM projects ORDER BY created_at DESC LIMIT $1 OFFSET $2",
+      [limit, offset],
+    );
+
+    const countResult = await pool.query("SELECT COUNT(*) FROM projects");
+    const totalProjects = parseInt(countResult.rows[0].count);
+
+    res.json({
+      totalProjects,
+      totalPages: Math.ceil(totalProjects / limit),
+      currentPage: page,
+      projects: allProjects.rows,
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Server error while fetching projects" });
+  }
+});
+
+app.post(
+  "/api/projects",
+  verifyToken,
+  upload.single("image"),
+  async (req, res) => {
+    try {
+      const { title, description, github_url } = req.body;
+      const image_url = req.file ? `/uploads/${req.file.filename}` : null;
+
+      const newProject = await pool.query(
+        "INSERT INTO projects (title, description, image_url, github_url) VALUES ($1, $2, $3, $4) RETURNING *",
+        [title, description, image_url, github_url],
+      );
+
+      res.status(201).json({
+        message: "Project added successfully with image",
+        project: newProject.rows[0],
+      });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ error: "Server error while adding project" });
+    }
+  },
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
